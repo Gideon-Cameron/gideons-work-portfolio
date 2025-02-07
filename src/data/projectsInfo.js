1518822413544a76f96ddf18a6fe17b87dd9a79a
Gideon Cameron <gideonwork20@gmail.com>
2025-02-07 01:00:35 -0800

    Fix image paths and descriptions for More Projects

--- src/data/projectsInfo.js
+++ src/data/projectsInfo.js
@@ -1,75 +1,60 @@
 const projectsInfo = [
-   {
-     id: 1,
-     title: "Fluentwave",
-     image: "/src/assets/Fluentwave.webp",
-     description: "Fluentwave is a language learning module. Including gamification features and user tracking to keep user learning and satisfied.",
-     liveLink: "https://fluentwave-beta.netlify.app/",
-   },
-    {
-      id: 2,
-      title: "Space Tourism",
-      image: "/src/assets/space-tourism.webp",
-      description: "Ever wanted to explore space? Here is your chance, check out locations and meet the crew, while you see the wonders that space travel brings.",
-      liveLink: "https://gideon-cameron.github.io/Space-Touris/",
-    },
-    {
-      id: 3,
-      title: "React-Weather-App",
-      image: "/src/assets/React-Weather-App.webp",
-      description: "Keep a track of your local weather conditions, or check the weather for a vacations site with this weather tracking app.",
-      liveLink: "https://gideon-cameron.github.io/React-Weather-App/",
-    },
-    {
-      id: 4,
-      title: "Calculator-App",
-      image: "/src/assets/Calculator.webp",
-      description: "Like counting? make it more fun with this calculator app with changeable themes.",
-      liveLink: "https://gideon-cameron.github.io/Calculator-app/",
-    },
-    {
-      id: 5,
-      title: "Landing Page",
-      image: "/src/assets/Landing-Page.webp",
-      description: "Need a landing page for your website? Get a fun and interactive design as featured in the live site.",
-      liveLink: "https://gideon-cameron.github.io/Intro-section-with-dropdown-navigation-solution/",
-    },
-    {
-      id: 6,
-      title: "FQA",
-      image: "/src/assets/FQA.webp",
-      description: "Have any questiuons that your custumers are frequently asking? How about a cusome made FQA",
-      liveLink: "https://gideon-cameron.github.io/FQA-accordion-challenge/",
-    },
-    {
-      id: 7,
-      title: "Recipe Page",
-      image: "/src/assets/Recipe-page.webp",
-      description: "Looking for a tasty new recipe to try out? Or maybe you want to impress a friend? Check out the recipe for a simple and tasty omelet.",
-      liveLink: "https://gideon-cameron.github.io/Recipe-page/",
-    },
-    {
-      id: 8,
-      title: "Advice-generator",
-      image: "/src/assets/Advice-generator.webp",
-      description: "Ever need some good advice? Or want to be the wise one in the group? Check out this advice generator.",
-      liveLink: "https://gideon-cameron.github.io/Advice-generator-app/",
-    },
-    // {
-    //   id: 9,
-    //   title: "Project 9",
-    //   image: "/src/assets/filler.jpg",
-    //   description: "This is a placeholder description for Project 9.",
-    //   liveLink: "https://gideon-cameron.github.io/Space-Touris/",
-    // },
-    // {
-    //   id: 10,
-    //   title: "Project 10",
-    //   image: "/src/assets/filler.jpg",
-    //   description: "This is a placeholder description for Project 10.",
-    //   liveLink: "https://gideon-cameron.github.io/Space-Touris/",
-    // },
-  ];
-  
-  export default projectsInfo;
-  
+  {
+    id: 1,
+    title: "Fluentwave",
+    image: "/assets/Fluentwave.webp",
+    description: "Fluentwave is a language learning module. Including gamification features and user tracking to keep users learning and satisfied.",
+    liveLink: "https://fluentwave-beta.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Space Tourism",
+    image: "/assets/space-tourism.webp",
+    description: "Ever wanted to explore space? Here is your chance, check out locations and meet the crew, while you see the wonders that space travel brings.",
+    liveLink: "https://gideon-cameron.github.io/Space-Touris/",
+  },
+  {
+    id: 3,
+    title: "React Weather App",
+    image: "/assets/React-Weather-App.webp",
+    description: "Keep track of your local weather conditions, or check the weather for a vacation site with this weather tracking app.",
+    liveLink: "https://gideon-cameron.github.io/React-Weather-App/",
+  },
+  {
+    id: 4,
+    title: "Calculator App",
+    image: "/assets/Calculator.webp",
+    description: "Like counting? Make it more fun with this calculator app with changeable themes.",
+    liveLink: "https://gideon-cameron.github.io/Calculator-app/",
+  },
+  {
+    id: 5,
+    title: "Landing Page",
+    image: "/assets/Landing-Page.webp",
+    description: "Need a landing page for your website? Get a fun and interactive design as featured in the live site.",
+    liveLink: "https://gideon-cameron.github.io/Intro-section-with-dropdown-navigation-solution/",
+  },
+  {
+    id: 6,
+    title: "FAQ",
+    image: "/assets/FQA.webp",
+    description: "Have any questions that your customers are frequently asking? How about a custom-made FAQ.",
+    liveLink: "https://gideon-cameron.github.io/FQA-accordion-challenge/",
+  },
+  {
+    id: 7,
+    title: "Recipe Page",
+    image: "/assets/Recipe-page.webp",
+    description: "Looking for a tasty new recipe to try out? Or maybe you want to impress a friend? Check out the recipe for a simple and tasty omelet.",
+    liveLink: "https://gideon-cameron.github.io/Recipe-page/",
+  },
+  {
+    id: 8,
+    title: "Advice Generator",
+    image: "/assets/Advice-generator.webp",
+    description: "Ever need some good advice? Or want to be the wise one in the group? Check out this advice generator.",
+    liveLink: "https://gideon-cameron.github.io/Advice-generator-app/",
+  },
+];
+
+export default projectsInfo;
